ddc3413ecb893a6dbaed47ad20275573bdb5ef32
Erik Morén <erik.moren78@gmail.com>
2024-09-02 10:20:51 +0200

    Uppgift 1 klar

--- WinFormsWeTeachIT/Form1.Designer.cs
+++ WinFormsWeTeachIT/Form1.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox = new ListBox();
+            panel1 = new Panel();
+            textBox = new TextBox();
+            Add = new Button();
+            Insert = new Button();
+            Delete = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listBox
+            // 
+            listBox.FormattingEnabled = true;
+            listBox.ItemHeight = 15;
+            listBox.Location = new Point(20, 20);
+            listBox.Name = "listBox";
+            listBox.Size = new Size(211, 169);
+            listBox.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox);
+            panel1.Controls.Add(Add);
+            panel1.Controls.Add(Insert);
+            panel1.Controls.Add(Delete);
+            panel1.Location = new Point(237, 20);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(286, 153);
+            panel1.TabIndex = 1;
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(8, 17);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(256, 23);
+            textBox.TabIndex = 3;
+            // 
+            // Add
+            // 
+            Add.Location = new Point(12, 58);
+            Add.Name = "Add";
+            Add.Size = new Size(75, 23);
+            Add.TabIndex = 2;
+            Add.Text = "Add";
+            Add.UseVisualStyleBackColor = true;
+            Add.Click += Add_Click;
+            // 
+            // Insert
+            // 
+            Insert.Location = new Point(12, 116);
+            Insert.Name = "Insert";
+            Insert.Size = new Size(75, 23);
+            Insert.TabIndex = 1;
+            Insert.Text = "Insert";
+            Insert.UseVisualStyleBackColor = true;
+            Insert.Click += Insert_Click;
+            // 
+            // Delete
+            // 
+            Delete.Location = new Point(12, 87);
+            Delete.Name = "Delete";
+            Delete.Size = new Size(75, 23);
+            Delete.TabIndex = 0;
+            Delete.Text = "Delete";
+            Delete.UseVisualStyleBackColor = true;
+            Delete.Click += Delete_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(listBox);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox listBox;
+        private Panel panel1;
+        private Button Insert;
+        private Button Delete;
+        private Button Add;
+        private TextBox textBox;
     }
 }
--- WinFormsWeTeachIT/Form1.cs
+++ WinFormsWeTeachIT/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace WinFormsWeTeachIT
 {
     public partial class Form1 : Form
@@ -5,6 +7,40 @@ namespace WinFormsWeTeachIT
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (sender is Button) 
+            {
+                listBox.Items.Add(textBox.Text);
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (listBox.SelectedItems.Count > 0 && listBox.SelectedItem != null)
+            {
+                listBox.Items.Remove(listBox.SelectedItem);
+            }
+            else
+            {
+                Debug.WriteLine("Du har inte valt något att ta bort");
+            }
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            if (listBox.SelectedItems.Count > 0 && listBox.SelectedItem != null) 
+            {
+                listBox.Items.Insert(listBox.SelectedIndex, textBox.Text);
+                textBox.Text = string.Empty;
+            }
+            else
+            {
+                Debug.WriteLine("Du har inte valt vart den ska läggas till");
+            }
         }
     }
 }
